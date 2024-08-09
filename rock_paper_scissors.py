41793e423420e5c1fda527e68512c4115c364d01
Michael Rozenberg <M.Rozzenberg@gmail.com>
2024-08-10 01:12:03 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,19 +29,14 @@ scissors = '''
 
 print("Welcome to the game!\n")
 game = [rock, paper, scissors]
-
 player = int(input("Choose:\n0 = rock\n1 = paper\n2 = scissors\n"))
-
 computer = random.randrange(0, 3)
-if player != 0:
-    print("You can choose only numbers 0, 1 and 2.")
-elif player != 1:
-    print("You can choose only numbers 0, 1 and 2.")
-elif player != 2:
-    print("You can choose only numbers 0, 1 and 2.")
+if player not in [0, 1, 2]:
+    print("You can choose only numbers 0, 1 or 2.")
+    exit()
+    
 print("Your choice:\n", game[player])
 print("Computer's choice:\n", game[computer])
-
 if player == 0 and computer == 2:
     print("You win!")
 elif player == 1 and computer == 0:
